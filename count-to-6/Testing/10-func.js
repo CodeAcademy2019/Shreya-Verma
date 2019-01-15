e2ef9651ce566584394d04b4f49ea9b3c3586189
Shreya Verma <svshreya97@gmail.com>
2019-01-15 14:43:07 +0530

    Added scope and closures

--- count-to-6/Testing/10-func.js
+++ count-to-6/Testing/10-func.js
@@ -2,7 +2,7 @@ var html=(...args) => {
     var safe={"'": '&apos;', '"': '&quot;', '<': "&lt;", ">": "&gt;", "&": "&amp;"};
     var b=args.slice(1).map((a) => a.replace(/"|'|<|>|&/g, (c) => safe[c]));
     var c=args[0].reduce((r, a, i)=>r+=(b[i-1]+a));
-    console.log(`${args} 
+    console.log(`${args}
 ${c}`);
     return c;
 }
