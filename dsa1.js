b849e6047e6d7228096e1182de63bb9c06d7261a
Shreya Verma <svshreya97@gmail.com>
2019-01-18 23:10:47 +0530

    Input function added

--- dsa1.js
+++ dsa1.js
@@ -6,72 +6,93 @@ const getIndices= (val, arr) => {
     }
     return indexes;
 }
-const returnRes=()=>{
 
-}
-const mainFunc= (...arrs)=> {
-    resultPos=[];
-    resultZero=[];
-    maxProfit=0;
-    arrs.forEach((arr)=> {
-        if (arr.length==1){
-            if (10-arr[0]>0) {
-                resultPos.push([1]);
-                maxProfit+=(10-arr[0]);
-            }
-            else if (10-arr[0]==0){
-                resultZero.push([1]);
-            }
+const inp=(str)=> {
+    res=str.split('\n');
+    ret=[];
+    for (i=0;i<res.length-1;++i) {
+        j=0;
+        temp=[];
+        temp2=Number(res[i]);
+        while(j<temp2){
+            ++i;
+            ++j;
+            temp.push((res[i].split(' ')).slice(1));
+            //console.log('temp:',temp);
         }
-        else if (arr.length>1){
-            stack=[10-arr[0]];
-            (arr.slice(1)).forEach((i, index) => {
-                stack.push((10-i)+stack[index]);
-            });
-            //console.log(stack);
-            if (Math.max(...stack)>0) {
-                resultPos.push(getIndices(Math.max(...stack), stack));
-                maxProfit+=Math.max(...stack);
-            }
-            else if (Math.max(...stack)==0) {
-                resultZero.push(getIndices(Math.max(...stack), stack));
-            }
-        }
-    });
-    //console.log('Result Pos:',resultPos,'Result Zero:', resultZero);
-    //console.log(maxProfit);
-    if (resultPos.length==0) {
-        return [0, 0];
+        ret=ret.concat([temp]);
+        
     }
-    final=resultPos[0];
-    //console.log(resultPos, final)
-    if (resultPos.length>1) {
-        (resultPos.slice(1)).forEach((val)=> {
+    //console.log(ret);
+    return ret;
+}
+const mainFunc= (str)=> {
+    arrsbig=inp(str);
+    retVal='';
+    k=0;
+    arrsbig.forEach((arrs)=> {
+        resultPos=[];
+        resultZero=[];
+        maxProfit=0;
+        arrs.forEach((arr)=> {
+            if (arr.length==1){
+                if (10-arr[0]>0) {
+                    resultPos.push([1]);
+                    maxProfit+=(10-arr[0]);
+                }
+                else if (10-arr[0]==0){
+                    resultZero.push([1]);
+                }
+            }
+            else if (arr.length>1){
+                stack=[10-arr[0]];
+                (arr.slice(1)).forEach((i, index) => {
+                    stack.push((10-i)+stack[index]);
+                });
+                //console.log(stack);
+                if (Math.max(...stack)>0) {
+                    resultPos.push(getIndices(Math.max(...stack), stack));
+                    maxProfit+=Math.max(...stack);
+                }
+                else if (Math.max(...stack)==0) {
+                    resultZero.push(getIndices(Math.max(...stack), stack));
+                }
+            }
+        });
+        //console.log('Result Pos:',resultPos,'Result Zero:', resultZero);
+        //console.log(maxProfit);
+        if (resultPos.length==0) {
+            return [0, 0];
+        }
+        final=resultPos[0];
+        //console.log(resultPos, final)
+        if (resultPos.length>1) {
+            (resultPos.slice(1)).forEach((val)=> {
+                temp=final;
+                final=[];
+                val.forEach((val2)=> {
+                    temp.forEach((val3)=> {
+                        final.push(val2+val3);
+                    });
+                });
+            })
+        }   
+        resultZero.forEach((val)=>{
             temp=final;
-            final=[];
             val.forEach((val2)=> {
                 temp.forEach((val3)=> {
                     final.push(val2+val3);
                 });
             });
-        })
-    }   
-    resultZero.forEach((val)=>{
-        temp=final;
-        val.forEach((val2)=> {
-            temp.forEach((val3)=> {
-                final.push(val2+val3);
-            });
         });
+        unique=new Set(final);
+        res=Array.from(unique);
+        res.sort((a, b) => a - b);
+        //console.log([maxProfit, res.slice(0,10)]);
+        retVal+=((k+1).toString()+'\n'+maxProfit.toString()+'\n'+(res.slice(0,10)).join(' ')+'\n');
+        ++k;
     });
-    unique=new Set(final);
-    res=Array.from(unique);
-    res.sort((a, b) => a - b);
-    console.log([maxProfit, res.slice(0,10)]);
-    return [maxProfit, res.slice];
- 
+    return retVal;
 }
-mainFunc([1, 2, 3, 10, 15, 5], [15, 5, 15, 5, 15, 5]);
-mainFunc([5, 13, 8, 7, 11, 9, 2, 10, 11, 13]);
-mainFunc([8, 1, 9, 15, 3, 6, 10], [7, 2, 14, 10]);
-mainFunc([3, 2, 1, 3, 10, 12, 2], [12, 13, 14, 15, 2, 3], [1, 2, 3])
+module.exports=mainFunc;
+
