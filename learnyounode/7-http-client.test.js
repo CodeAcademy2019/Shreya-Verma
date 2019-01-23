cf0f6ff9f6eb775463ae16866e564e120484cf40
Shreya Verma <svshreya97@gmail.com>
2019-01-23 09:58:23 +0530

    Added seventh exercise

---
+++ learnyounode/7-http-client.test.js
@@ -0,0 +1,19 @@
+const getClient= require('./7-http-client');
+describe('getClient()', () => {
+    it ('should receive info from http URL', (done) => {
+        function callbackF(data){
+            expect(data).toEqual('Hello World\n');
+            done();
+        }
+        process.argv = ['node', '7-http-client.js','http://localhost:7000'];
+        getClient(callbackF);
+    });    
+    it ('should not receive anything other than info from http URL', (done) => {
+        function callbackF(data){
+            expect(data).not.toEqual('hello world\n');
+            done();
+        }
+        process.argv = ['node', '7-http-client.js','http://localhost:7000'];
+        getClient(callbackF);
+    });    
+});
