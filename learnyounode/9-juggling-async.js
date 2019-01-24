a758644e46c66269e15735c580205da35fd84be0
Shreya Verma <svshreya97@gmail.com>
2019-01-24 10:45:14 +0530

    Added ninth exercise

---
+++ learnyounode/9-juggling-async.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const juggleAsync = () => {
+    const urlArr = process.argv.slice(2);
+    const contentArr = [];
+    urlArr.forEach(() => {
+        contentArr.push(null);
+    }); 
+    urlArr.forEach((url, index) => {
+        http.get(url, (response) => {
+            let content = '';
+            response.setEncoding('utf-8');
+            response.on('data', (data) => {
+                content += data;
+            });
+            response.on('end', () => {
+               contentArr[index] = content;
+               if (contentArr.filter((val) => val != null).length === urlArr.length){
+                   contentArr.forEach((val) => {
+                       console.log(val);
+                   });
+               } 
+            });
+        });
+    });
+};
+juggleAsync();
