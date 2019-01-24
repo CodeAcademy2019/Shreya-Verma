4121274391ff0ceef01d0b3f7aab500ebd8b4dda
Shreya Verma <svshreya97@gmail.com>
2019-01-24 10:45:44 +0530

    Added twelfth & thirteenth exercise

---
+++ learnyounode/12-http-uppercaser.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const map = require('through2-map');
+const timeServer = () => {
+    http.createServer((request, response) => {
+        if (request.method === 'POST') {
+            request.pipe(map(chunk => chunk.toString().toUpperCase()))
+                    .pipe(response);
+        }
+    }).listen(process.argv[2]);
+}
+timeServer();
---
+++ learnyounode/13-json-api-server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const url = require('url');
+const parsetime = (urlObj) => {
+  const date = new Date (urlObj.query.iso);
+  const dateObj = { 'hour' : date.getHours(),
+                    'minute' : date.getMinutes(),
+                    'second' : date.getSeconds()
+                  } 
+  return JSON.stringify(dateObj);
+};
+const unixtime = (urlObj) => {
+  const date = new Date (urlObj.query.iso);
+  const dateObj = { 'unixtime' : date.getTime()
+                  }
+  return JSON.stringify(dateObj);
+};
+const jsonTimeServer = () => {
+  const server = http.createServer((function(request,response)
+  { response.setHeader('Content-Type', 'text/plain');
+    urlObj = url.parse(request.url, true);
+    if (urlObj.pathname === '/api/parsetime'){
+      response.write(parsetime(urlObj)); 
+    } else if (urlObj.pathname ==='/api/unixtime'){
+      response.write(unixtime(urlObj));
+    }
+    response.end(); 
+  })).listen(process.argv[2]);
+};
+jsonTimeServer();
