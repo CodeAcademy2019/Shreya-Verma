e4fc9cbef04f9fefbc0467a346d47fe04e8625d1
Shreya Verma <svshreya97@gmail.com>
2019-01-25 11:00:28 +0530

    Added thirteenth test

--- learnyounode/13-json-api-server.test.js
+++ learnyounode/13-json-api-server.test.js
@@ -1,39 +1,40 @@
-// const http = require('http');
+const http = require('http');
 
-// describe('jsonApiServer()', () => {
-//   it('server should send back post request content in uppercase', (done) => {
-//     function callbackF(data) {
-//       console.log(data);
-//       expect(data.toString()).toEqual({"hour":17,"minute":40,"second":15});
-//       done();
-//     }
-//     const req = http.request({
-//       host: '127.0.0.1',
-//       port: 7000,
-//       path: '/api/parsetime?iso=2013-08-10T12:10:15.474Z'
-//     }, (response) => {
-//       response.setEncoding('utf8');
-//       response.on('data', callbackF);
-//       response.on('error', console.log);
-//     });
-//     req.write();
-//     req.end();
-//   });
-//   xit('server should not send back anything other than post request content in uppercase', (done) => {
-//     function callbackF(data) {
-//       console.log(data);
-//       expect(data).not.toEqual('hello');
-//       done();
-//     }
-//     const req = http.request({
-//       host: '127.0.0.1',
-//       port: 7000,
-//       method: 'POST',
-//     }, (response) => {
-//       response.setEncoding('utf8');
-//       response.on('data', callbackF);
-//       response.on('error', console.log);
-//     });
-//     req.write('hello');
-//   });
-// });
+describe('jsonApiServer()', () => {
+  it('server should send back hour minute second object', (done) => {
+    function callbackF(data) {
+      console.log(data);
+      expect(JSON.parse(data)).toEqual({"hour":17,"minute":40,"second":15});
+      done();
+    }
+    const req = http.request({
+      method: 'GET',
+      host: '127.0.0.1',
+      port: 7000,
+      path: '/api/parsetime?iso=2013-08-10T12:10:15.474Z'
+    }, (response) => {
+      response.setEncoding('utf8');
+      response.on('data', callbackF);
+      response.on('error', console.log);
+    });
+    req.end();
+  });
+  it('server should send back unixtime object', (done) => {
+    function callbackF(data) {
+      console.log(data);
+      expect(JSON.parse(data)).toEqual({"unixtime":1376136615474});
+      done();
+    }
+    const req = http.request({
+      method: 'GET',
+      host: '127.0.0.1',
+      port: 7000,
+      path: '/api/unixtime?iso=2013-08-10T12:10:15.474Z'
+    }, (response) => {
+      response.setEncoding('utf8');
+      response.on('data', callbackF);
+      response.on('error', console.log);
+    });
+    req.end();
+  });
+});
