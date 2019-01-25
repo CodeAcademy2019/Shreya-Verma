a909f1f464863c0716c9579d77ed7d9cd7818f57
Shreya Verma <svshreya97@gmail.com>
2019-01-25 09:02:11 +0530

    Tested tenth exercise

--- learnyounode/10-time-server.js
+++ learnyounode/10-time-server.js
@@ -1,32 +1,34 @@
 const net = require('net');
+
 const addZero = (val) => {
-    if (val<10){
-        return '0'+val;
-    }
-    return val;
+  if (val < 10) {
+    return `0${val}`;
+  }
+  return val;
 };
 const callback = (res) => {
-    console.log(res);
-}
-const createDate = () => {
-    date = new Date();
-    data = date.getFullYear() + '-' +
-            addZero(date.getMonth() + 1) + '-' +    
-            addZero(date.getDate()) + ' ' + 
-            addZero(date.getHours()) + ':' +
-            addZero(date.getMinutes()) + '\n';
-    return data;
-}
-const timeServer = (callback) => {
-    var data;
-    const server = net.createServer((socket) => {
-        data=createDate()
-        socket.write(data);
-        socket.on('data', callback);
-        socket.end();
-    });
-    server.listen(process.argv[2]);
-    //server.close();
+  console.log(res);
 };
-timeServer(callback);
-module.exports = {timeServer, createDate};
+const createDate = () => {
+  const date = new Date();
+  const data = `${date.getFullYear()}-${
+    addZero(date.getMonth() + 1)}-${
+    addZero(date.getDate())} ${
+    addZero(date.getHours())}:${
+    addZero(date.getMinutes())}\n`;
+  //console.log('data', data);
+  return data;
+};
+const timeServer = (callbackF) => {
+  let data;
+  const server = net.createServer((socket) => {
+    data = createDate();
+    socket.write(data);
+    //socket.end();
+  });
+  server.listen(7000);
+  //server.close();
+};
+//timeServer(callback);
+
+module.exports = { timeServer, createDate };
--- learnyounode/10-time-server.test.js
+++ learnyounode/10-time-server.test.js
@@ -1,12 +1,22 @@
-const timeServerMain= require('./10-time-server');
-describe('timeServer()', () => {
-    it ('should write date time to server socket', (done) => {
-        function callbackF(data){//, done){
-            expect(data).toEqual('abc')//timeServerMain.createDate());
-            //done();
-        }
-        process.argv = ['node', '10-time-server.js',7000];
-        timeServerMain.timeServer(callbackF);
-        done();
-    });    
+const net = require('net');
+const timeServerMain = require('./10-time-server');
+describe('checking time server', () => {
+  it('should receive date from port', (done) => {
+    function callbackF(data) {
+      console.log(data.toString());
+      expect(data.toString()).toEqual(timeServerMain.createDate());
+      done();
+    }
+    const client = net.connect(7000);
+    client.on('data', callbackF);
+  });
+  it('should not receive anything other than date from port', (done) => {
+    function callbackF(data) {
+      console.log(data.toString());
+      expect(data.toString()).not.toEqual('sdhcvdsv');
+      done();
+    }
+    const client = net.connect(7000);
+    client.on('data', callbackF);
+  });
 });
